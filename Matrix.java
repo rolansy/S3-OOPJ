2a9f8dee488399ee296c92ec383a700104d5b98c
rolansy <ronalshoeygeorge@gmail.com>
2023-10-10 00:51:28 +0530

    Added Funcion to input matrix for example Matrix A

--- Matrix.java
+++ Matrix.java
@@ -11,12 +11,30 @@ public class Matrix{
         System.out.print("Enter the number of Columns of matrix B : ");
         int acol=sc.nextInt();
 
-        System.out.println("Enter the number of ROws of Matrix B : ");
+        System.out.print("Enter the number of ROws of Matrix B : ");
         int brow=sc.nextInt();
-        System.out.println("Enter the number of Columns of Matrix B : ");
+        System.out.print("Enter the number of Columns of Matrix B : ");
         int bcol=sc.nextInt();
+        
+        if (acol!=brow){
+            System.out.print("The Matrices cannot be multiplied : ");
+            return;
+        }
+
+        int[][] matrixA = new int[arow][acol]
+        System.out.println("Enter Elements for Matrix A : ");
+        Inputm(sc, matrixA);
+
         
 
     }
     
+    private static void Inputm(Scanner sc, int[][] matrix){
+        for (i=0;i<matrix.length;i++){
+            for(j=0;j<matrix.length;j++){
+                System.out.print("Enter Element at position ("+i+","+j+")");
+                matrix[i][j]=sc.nextInt()
+            }
+        }
+    }
 }
