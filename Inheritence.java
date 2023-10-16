b1e5cc72312fdf9f737f0797db77a957f18737f0
rolansy <ronalshoeygeorge@gmail.com>
2023-10-17 00:21:08 +0530

    Vehicle drive

---
+++ Inheritence.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Vehicle{
+    String brand;
+
+    public void drive(){
+        System.out.println("Driving the vehicle");
+    }
+
+}
+
+public class Inheritence {
+    public static void main(String[] args){
+
+
+    }
+}
