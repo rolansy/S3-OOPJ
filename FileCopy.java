690ec142775a375d66b75dc5cf132f99cca9672a
rolansy <ronalshoeygeorge@gmail.com>
2023-11-07 00:11:50 +0530

    File Copy program init

---
+++ FileCopy.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class FileCopy {
+    public static void main(String[] args) throws IOException {
+        FileInputStream f1=null;
+        FileOutputStream f2=null;
+        
+    }
+}
