fe669166f33e60ce18a6a5fb0bd6b2352622b07e
rolansy <ronalshoeygeorge@gmail.com>
2023-11-06 23:56:45 +0530

    Final fixes

--- ZeroDivException.java
+++ ZeroDivException.java
@@ -12,11 +12,12 @@ public class ZeroDivException {
             int c=a/b;
             System.out.println("The result is: "+c);
         }
-        catch(AirthmeticException e){
+        catch(ArithmeticException e){
             System.out.println("Cannot divide by zero");
         }
         finally{
             System.out.println("Program ends");
+            sc.close();
         }
     }
 }
