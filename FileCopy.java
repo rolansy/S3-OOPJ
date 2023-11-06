0cac767d6c40844c0818aa0cab8ad55b28c88e53
rolansy <ronalshoeygeorge@gmail.com>
2023-11-07 00:15:23 +0530

    Copy from trial to copy file

--- FileCopy.java
+++ FileCopy.java
@@ -3,6 +3,16 @@ public class FileCopy {
     public static void main(String[] args) throws IOException {
         FileInputStream f1=null;
         FileOutputStream f2=null;
-        
+        try{
+            f1=new FileInputStream("Trial.txt");
+            f2= new FileOutputStream("Copy.txt");
+            int c;
+            do{
+                c=f1.read();
+                if(c!=-1){
+                    f2.write((char)c);
+                }while (c!=-1);
+            }
+        }
     }
 }
