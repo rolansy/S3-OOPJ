e13839ae02fbbd4374b7f543cc230ed6f4e998e5
rolansy <ronalshoeygeorge@gmail.com>
2023-11-17 14:21:49 +0300

    try copy and merging

--- tutorial/CopyAppendMerge.java
+++ tutorial/CopyAppendMerge.java
@@ -10,5 +10,23 @@ public class CopyAppendMerge{
         BufferedReader br1=new BufferedReader(new FileReader(f1));
         BufferedReader br2=new BufferedReader(((new FileReader(f2));
         BufferedWriter bmw=new BufferedWriter(new FileWriter(fm));
+
+        String c;
+
+        while((c=br1.readLine())!=null){
+            bmw.write(c);
+            bmw.newLine();
+        }
+
+        while((c=br2.readLine())!=null){
+            bmw.write(c);
+            bmw.newLine();
+        }
+
+        br1.close();
+        br2.clode();
+        bmw.close();
+
+        System.out.print("Files Merged Successfully");
     }
 }
