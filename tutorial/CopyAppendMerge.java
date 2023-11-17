80f04fa8651b6d2c12d8f8d0693c614fb367b586
rolansy <ronalshoeygeorge@gmail.com>
2023-11-17 14:31:27 +0300

    Fix file paths and add exception handling

--- tutorial/CopyAppendMerge.java
+++ tutorial/CopyAppendMerge.java
@@ -2,31 +2,35 @@ package tutorial;
 import java.io.*;
 public class CopyAppendMerge{
     public static void main(String[] args) {
-        String f1="File1.txt";
-        String f2="File2.txt";
-        String fm="Merged.txt";
-    }
-    try{
-        BufferedReader br1=new BufferedReader(new FileReader(f1));
-        BufferedReader br2=new BufferedReader(((new FileReader(f2));
-        BufferedWriter bmw=new BufferedWriter(new FileWriter(fm));
+        String f1="D:\\java\\S3 OOPJ\\tutorial\\File1.txt";
+        String f2="D:\\java\\S3 OOPJ\\tutorial\\File2.txt";
+        String fm="D:\\java\\S3 OOPJ\\tutorial\\Merged.txt";
+        try{
+            BufferedReader br1=new BufferedReader(new FileReader(f1));
+            BufferedReader br2=new BufferedReader(new FileReader(f2));
+            BufferedWriter bmw=new BufferedWriter(new FileWriter(fm));
 
-        String c;
+            String c;
 
-        while((c=br1.readLine())!=null){
-            bmw.write(c);
-            bmw.newLine();
+            while((c=br1.readLine())!=null){
+                bmw.write(c);
+                bmw.newLine();
+            }
+
+            while((c=br2.readLine())!=null){
+                bmw.write(c);
+                bmw.newLine();
+            }
+
+            br1.close();
+            br2.close();
+            bmw.close();
+
+            System.out.print("Files Merged Successfully");
         }
-
-        while((c=br2.readLine())!=null){
-            bmw.write(c);
-            bmw.newLine();
+        catch (IOException e ){
+            System.out.println("File not found");
+            System.out.println(e);
         }
-
-        br1.close();
-        br2.clode();
-        bmw.close();
-
-        System.out.print("Files Merged Successfully");
     }
 }
