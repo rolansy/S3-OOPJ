7387b05c5bf93bca6a4bc311445545ed415c5548
rolansy <ronalshoeygeorge@gmail.com>
2023-11-17 11:32:12 +0300

    try buff reader

--- tutorial/CopyAppendMerge.java
+++ tutorial/CopyAppendMerge.java
@@ -4,6 +4,11 @@ public class CopyAppendMerge{
     public static void main(String[] args) {
         String f1="File1.txt";
         String f2="File2.txt";
-        String fmerge="Merged.txt";
+        String fm="Merged.txt";
+    }
+    try{
+        BufferedReader br1=new BufferedReader(new FileReader(f1));
+        BufferedReader br2=new BufferedReader(((new FileReader(f2));
+        BufferedWriter bmw=new BufferedWriter(new FileWriter(fm));
     }
 }
